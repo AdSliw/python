82e33071aa7af639cf118af40eb36be8c1ecff0e
Adam Śliwiński <adam.sliwinski.git@gmail.com>
2023-01-23 18:15:21 +0100

    UAM

---
+++ ex03_variable_assign_v2.py
@@ -0,0 +1,12 @@
+'''
+Exercise 03
+Assign two variables (you can use any names). 
+The first  should store text: „Python”. 
+The second one: text: „3.8”
+Using these variables and print() function, print to the terminal:
+
+I learn Python ver. 3.8
+'''
+a = "Python"
+b = "3.8"
+print("I learn", a, "ver.", b)
---
+++ ex04_multiple_prints.py
@@ -0,0 +1,14 @@
+'''
+Using 3 x print() functions, 
+Print to terminal :
+---------------
+Static program, ver. 1.002
+---------------
+
+(let's assume you have 15 dashes)
+
+'''
+
+print(15*"-")
+print("Static program, ver. 1.002")
+print(15*"-")
