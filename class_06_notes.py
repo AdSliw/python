a80c155bc753bd25a46c4985f130ae2d372bd8c9
AdSliw <118202246+AdSliw@users.noreply.github.com>
2023-02-01 02:40:32 +0100

    UAM

---
+++ class_06_notes.py
@@ -0,0 +1,61 @@
+# Class_06
+
+# WINDOWS COMMANDS
+# change directory
+# cd 
+# dir 
+# echo #creating files
+# mkdir #creating folders
+
+# print("Hello world!")
+# \n means NEW LINE
+# print("Ana", "has", "a cat", end=" ")
+# print("and", "a dog")
+
+"""
+multiline
+comment
+"""
+
+# cd Documents
+# mkdir UAM1
+# echo Hello > First.txt
+# echo World >> First.txt
+# type First.txt
+
+# print(type(3))
+# print(type(3.17))
+# print(type(True))
+
+# print(2 + 2)
+# print(x + y * 3)
+# print((x + y) * 2)
+
+# x = 5
+# x = x+6
+# x += 6
+# print(x)
+
+# i = 7
+# j = 5.0
+# i = i + j
+# i += j
+
+
+# print(i)
+# print(i-j)
+# print(i*j)
+# print(i/j)
+# print(i**j)
+# print(i%j)
+# print(i//j)
+
+# COMPARISON OPERATORS
+
+# print(i>j)
+# print(j>i)
+
+# print(i>=j)
+# print(i<=j)
+# print(i==j)
+# print(i!=j)
---
+++ class_08_notes.py
@@ -0,0 +1,25 @@
+# Class_08
+
+meal = "fruit"
+money = 1 
+is_lunch_delivered = (meal == "fruit" or meal == "sandwich") and money >= 2
+print(is_lunch_delivered)
+
+
+"""
+if boolean expression:
+    block of code
+else:
+    block of code
+    
+"""
+
+# Example:
+
+temperature = int(input("input your temperature:\n"))
+if (temperature >= 38):
+    print("stay home")
+else:
+    print("do as usual")
+
+
