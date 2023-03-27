905dd602fa6f9bf7ade691e24e4cf68c4ed0912b
AdSliw <118202246+AdSliw@users.noreply.github.com>
2023-03-27 20:01:50 +0200

    sem2

--- sem2/ex12_palindrome_recursive.py
+++ sem2/ex12_palindrome_recursive.py
@@ -70,64 +70,10 @@ def print_palindrome_check(text: str) -> None:
     else:
         print(f"{text} is not a palindrome.")
 
-print_palindrome_check("Dad") # output: Dad is a palindrome.
-print_palindrome_check("Evil olive.") # output: evilolive is a palindrome.
-print_palindrome_check("Never odd or even.") # output: neveroddoreven is a palindrome.
-print_palindrome_check("Amore, Roma.") # output: amoreroma is a palindrome.
-print_palindrome_check("test") # output: test is not a palindrome.
-print_palindrome_check("ad") # output: ad is a palindrome.
-print_palindrome_check("a") # output: a is a palindrome.
-
-
-'''
-
-Exercise 90, file pp_90.py
-Write a program that will check if a word/sentence is a palindrome or not.
-Remove punctuations and spaces
-Use iterative way (loop) to check if palindrome
-
-These are palindromes, test with them:
-Dad 
-Evil olive.
-Never odd or even.
-Amore, Roma.
-
-Not palindromes:
-test
-ad
-a
-
-'''
-
-# from string import punctuation as punct
-
-# def is_palindrome(text: str) -> bool:
-   
-#    '''
-#    Function checks if any text is palindrome or not
-
-#    Parameters:
-#    text: any text
-#    Returns:
-#    True: if text is palindrome
-#    False: if text is not a palindrome
-#    '''
-# # your code below
-#    n = ''
-#    text = text.replace(' ','').lower()
-#    text = ''.join(char for char in text if char not in punct)
-#    for w in text:
-#       n = w + n
-#    if n == text:
-#       return True
-#    else:
-#       return False
-      
-
-# print(is_palindrome('dad'))
-# print(is_palindrome('mama'))
-# print(is_palindrome('Evil olive'))
-# print(is_palindrome('test'))
-# print(is_palindrome('Never odd or even'))
-# print(is_palindrome('Amore, Roma'))
-
+print_palindrome_check("Dad")
+print_palindrome_check("Evil olive.")
+print_palindrome_check("Never odd or even.")
+print_palindrome_check("Amore, Roma.")
+print_palindrome_check("test")
+print_palindrome_check("ad")
+print_palindrome_check("a")
